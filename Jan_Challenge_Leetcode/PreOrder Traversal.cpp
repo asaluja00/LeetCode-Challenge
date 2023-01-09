602ac864167bbd3f2d81f34d649d2fa0558f6d87
Anmol Saluja <asaluja00@gmail.com>
2023-01-09 11:43:45 +0530

    9th Jan Challenge

--- Jan_Challenge_Leetcode/PreOrder Traversal.cpp
+++ Jan_Challenge_Leetcode/PreOrder Traversal.cpp
@@ -1,3 +1,6 @@
+
+//recursive approach 
+
 class Solution {
 public:
     void print(TreeNode* ptr , vector<int> &a){
@@ -11,6 +14,29 @@ public:
         vector<int> a;
         if(root==NULL) return a;
         print(root,a);
+        return a;
+    }
+};
+
+
+// Iterative Approach
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> a;
+        stack<TreeNode*> stk;
+        while(root || !stk.empty()){
+            if(root){
+                a.push_back(root->val);
+                if(root->right) stk.push(root->right);
+                root=root->left;
+            }
+            else{
+                root=stk.top();
+                stk.pop();
+            }
+        }
         return a;
     }
 };
